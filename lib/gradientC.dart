c80bed2a3633f4a49465c59ce054aa7bfa42869c
lucie <lu@>
2024-05-05 16:14:28 +0200

    button

--- lib/gradientC.dart
+++ lib/gradientC.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
-import 'styledText.dart';
+//import 'styledText.dart';
 
 class GradientC extends StatelessWidget {
   const GradientC({super.key});
 
+  void rollDice() {}
+
   @override
   Widget build(context) {
     return Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 167, 61, 61),
-            Color.fromARGB(255, 71, 8, 8)
-          ]),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 167, 61, 61),
+          Color.fromARGB(255, 71, 8, 8)
+        ]),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              child: const Text('rollen'),
+            )
+          ],
         ),
-        child: StyledText("00HALLOOOO")); //const Center(child: Text("gklf1")));
+      ),
+    );
+    //StyledText("00HALLOOOO")); //const Center(child: Text("gklf1")));
   }
 }
